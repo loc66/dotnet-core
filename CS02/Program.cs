cdf9a865a9ef32354d1962369900d667fd95e9bd
loc66 <dhloc.se@gmail.com>
2023-11-04 14:14:15 +0700

    CS02 - Biến, hằng số, kiểu dữ liệu

---
+++ CS02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS02 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("loc ddaay");
+        }
+    }
+}
