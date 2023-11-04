5fc8f7e68d65868642f0de0133f73893422bf5fc
loc66 <dhloc.se@gmail.com>
2023-11-04 13:42:55 +0700

    CS01

---
+++ CS01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello C#!");
+        }
+    }
+}
